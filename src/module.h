3293c5a2f3768e45e93b2263c1615fca260f3393
Rasmus Rendal <rasmus@rend.al>
2020-04-16 08:38:14 +0200

    Make veerything pass the CI

--- src/module.h
+++ src/module.h
@@ -1,8 +1,8 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <tuple>
 #include <map>
+#include <string>
+#include <tuple>
+#include <vector>
 
 using specie = std::string;
 using speciesRatios = std::map<specie, int>;
@@ -10,14 +10,14 @@ using reactionRate = int;
 using reaction = std::tuple<speciesRatios, speciesRatios, reactionRate>;
 
 class Module {
-	public:
-		Module() {}
-		std::string Compile();
+public:
+	Module() {}
+	std::string Compile();
 
-		std::string name;
-		std::vector<specie> inputSpecies;
-		std::vector<specie> outputSpecies;
-		std::vector<specie> species;
-		std::map<specie, int> concentrations;
-		std::vector<reaction> reactions;
+	std::string name;
+	std::vector<specie> inputSpecies;
+	std::vector<specie> outputSpecies;
+	std::vector<specie> species;
+	std::map<specie, int> concentrations;
+	std::vector<reaction> reactions;
 };
