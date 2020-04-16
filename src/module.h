ffe6d5e763b2227ab92dd5b5ef283f0754d0e919
Rasmus Rendal <rasmus@rend.al>
2020-04-16 09:41:27 +0200

    Added some basic module errors

--- src/module.h
+++ src/module.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <map>
+#include <set>
 #include <string>
 #include <tuple>
 #include <vector>
@@ -9,15 +10,26 @@ using speciesRatios = std::map<specie, int>;
 using reactionRate = int;
 using reaction = std::tuple<speciesRatios, speciesRatios, reactionRate>;
 
+struct SpecieNotDeclaredException : public std::exception {
+	std::string error;
+	SpecieNotDeclaredException(std::string speciesName, std::string moduleName)
+			: error("The species " + speciesName + " was not declared in module " +
+							moduleName) {}
+	const char *what() const throw() {
+		return error.c_str();
+	}
+};
+
 class Module {
 public:
 	Module() {}
+	void Verify();
 	std::string Compile();
 
 	std::string name;
 	std::vector<specie> inputSpecies;
 	std::vector<specie> outputSpecies;
-	std::vector<specie> species;
+	std::vector<specie> privateSpecies;
 	std::map<specie, int> concentrations;
 	std::vector<reaction> reactions;
 };
--- tests/moduletest.cpp
+++ tests/moduletest.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
 #include "module.h"
 #include "driver.h"
+#include <gtest/gtest.h>
 #include <iostream>
 
 class ModuleTest : public ::testing::Test {
@@ -16,9 +16,9 @@ protected:
 TEST_F(ModuleTest, twoValues) {
 	Module m;
 	m.name = "main";
-	m.species.push_back("a");
-	m.species.push_back("b");
-	m.species.push_back("c");
+	m.privateSpecies.push_back("a");
+	m.privateSpecies.push_back("b");
+	m.privateSpecies.push_back("c");
 
 	{
 		speciesRatios leftSide;
@@ -43,10 +43,29 @@ TEST_F(ModuleTest, twoValues) {
 	m.concentrations.insert(std::make_pair("b", 30));
 
 	std::string output = "main_a := 50;\n"
-		"main_b := 30;\n"
-		"main_a + main_b -> main_a + main_b + main_c;\n"
-		"main_c -> 0;\n";
+											 "main_b := 30;\n"
+											 "main_a + main_b -> main_a + main_b + main_c;\n"
+											 "main_c -> 0;\n";
 
 	EXPECT_EQ(m.Compile(), output);
+}
 
+TEST_F(ModuleTest, SpecieNotDeclared) {
+	Module m;
+	m.name = "main";
+	m.concentrations.insert(std::make_pair("a", 50));
+	EXPECT_THROW(m.Verify(), SpecieNotDeclaredException);
+}
+
+TEST_F(ModuleTest, SpecieNotDeclaredConcentrations) {
+	Module m;
+	m.name = "main";
+	{
+		speciesRatios leftSide;
+		leftSide.insert(std::make_pair("c", 1));
+		speciesRatios rightSide;
+		reaction r(leftSide, rightSide, 1);
+		m.reactions.push_back(r);
+	}
+	EXPECT_THROW(m.Verify(), SpecieNotDeclaredException);
 }
