c7c6257257c19c5a0582a2069d14a7c961d9c31c
Andrikz <andershb1234@gmail.com>
2020-04-21 11:59:45 +0200

    add enum for errors and function helper

--- src/frontend.h
+++ src/frontend.h
@@ -5,10 +5,16 @@
 #include <stdlib.h>
 #include <string>
 
+enum Error {
+	helpArgument = 0,
+	fileError = 1,
+};
+
 class Frontend {
 public:
 	driver *drv;
 	std::stringstream stream;
+	static void Helper(Error errorCode = helpArgument);
 	void GenerateStringStream();
 	void WriteFile();
 	std::string outputFileName = "out.crn";
