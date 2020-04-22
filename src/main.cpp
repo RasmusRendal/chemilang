bfc553d26d3c0520ca166cb89ae7f80ac004f900
Andrikz <andershb1234@gmail.com>
2020-04-22 15:50:48 +0200

    add check for if outfile is empty after -o

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "driver.h"
 #include "frontend.h"
+#include <cstdio>
 #include <iostream>
 #include <string>
 
@@ -16,16 +17,21 @@ int main(int argc, char *argv[]) {
 		Frontend::PrintHelper();
 		return 0;
 	}
+
 	for (int i = 1; i < argc; ++i) {
 		if (file_included(argv[i])) {
 			filename = argv[i];
-		} else if (argv[i] == std::string("-o")) {
+		} else if (argv[i] == std::string("-o") && argc == 4) {
 			frontend.outputFileName = std::string(argv[++i]);
+		} else if (argv[i] == std::string("-o") && argc == 3) {
+			Frontend::Exception(outFileError, argv[i]);
+			return 1;
 		} else {
 			Frontend::Exception(argError, argv[i]);
 			return 1;
 		}
 	}
+
 	if (drv.parse_file(filename) == 0) {
 		frontend.drv = &drv;
 		frontend.WriteFile();
