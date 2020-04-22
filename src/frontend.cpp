7253ced34e36b77ad1d3c6df51c0c5ff9be09b42
Andrikz <andershb1234@gmail.com>
2020-04-22 08:27:18 +0200

    Add Function Exception

--- src/frontend.cpp
+++ src/frontend.cpp
@@ -12,13 +12,21 @@ void Frontend::WriteFile() {
 	std::cout << "Output written to " << outputFileName << std::endl;
 }
 
-void Frontend::Helper(Error errorCode) {
+void Frontend::Exception(Error errorCode, const std::string &input) {
 	if (errorCode == fileError) {
 		std::cout << "Error: No file for parsing" << std::endl;
+	} else if (errorCode == argError) {
+		std::cout << "Invalid command-line argument used " << input << '\n';
+		std::cout << "Please see -h for more info" << std::endl;
 	}
-	std::string helperstring = "Usage:  chemilang filename [OPTIONS]\n"
-														 "Options:\n"
-														 "    -o  Output filename\n"
-														 "    -h  Display help information";
-	std::cout << helperstring << std::endl;
+}
+
+void Frontend::Helper(Error errorCode) {
+	if (errorCode == helpArgument) {
+		std::string helperstring = "Usage:  chemilang filename [OPTIONS]\n"
+                                            "Options:\n"
+                                            "    -o  Output filename\n"
+                                            "    -h  Display help information";
+		std::cout << helperstring << std::endl;
+	}
 };
