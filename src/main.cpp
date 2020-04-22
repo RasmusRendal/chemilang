3a18f253ac68bf321c4dab68cd91c21003c3e549
Andrikz <andershb1234@gmail.com>
2020-04-22 16:00:47 +0200

    change < to <= 3

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
 			filename = argv[i];
 		} else if (argv[i] == std::string("-o") && argc == 4) {
 			frontend.outputFileName = std::string(argv[++i]);
-		} else if (argv[i] == std::string("-o") && argc == 3) {
+		} else if (argv[i] == std::string("-o") && argc <= 3) {
 			Frontend::Exception(outFileError, argv[i]);
 			return 1;
 		} else {
