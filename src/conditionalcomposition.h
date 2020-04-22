393459894ebc732b75a5882e985d9cd9b44b04c3
Rasmus Rendal <rasmus@rend.al>
2020-04-22 12:53:29 +0200

    Add some documentation

--- src/conditionalcomposition.h
+++ src/conditionalcomposition.h
@@ -1,8 +1,18 @@
 #pragma once
 #include "composition.h"
 
+/*! \brief A composition in which the reactions have a catalyst
+ * \detail This type of composition allows for the user to have their
+ * composition in a format `if (specie) { compositions }`, where all
+ * compositions will have `specie` as a catalyst
+ */
 class ConditionalComposition : public Composition {
 public:
+	/** Constructor
+	 *
+	 * @param condition The specie which is added as acatalyst
+	 * @param subCompositions The child compositions
+	 */
 	ConditionalComposition(specie condition,
 												 std::vector<Composition *> subCompositions)
 			: condition(condition), subCompositions(subCompositions) {}
