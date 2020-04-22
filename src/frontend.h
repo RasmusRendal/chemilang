504896303cb81398d10390f5a43aee6ac6bf5c85
Andrikz <andershb1234@gmail.com>
2020-04-22 08:50:58 +0200

    clang Format

--- src/frontend.h
+++ src/frontend.h
@@ -8,7 +8,7 @@
 enum Error {
 	helpArgument = 0,
 	fileError = 1,
-        argError = 2,
+	argError = 2,
 };
 
 class Frontend {
@@ -16,7 +16,7 @@ public:
 	driver *drv;
 	std::stringstream stream;
 	static void Helper(Error errorCode = helpArgument);
-        static void Exception(Error errorCode, const std::string &input);
+	static void Exception(Error errorCode, const std::string &input);
 	void GenerateStringStream();
 	void WriteFile();
 	std::string outputFileName = "out.crn";
