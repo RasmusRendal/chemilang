d29d50e6b1297de4034190fe77dca2c3968fdfcf
Rasmus Rendal <rasmus@rend.al>
2020-04-24 09:14:02 +0200

    Fixed last issues with PR

--- src/main.cpp
+++ src/main.cpp
@@ -22,17 +22,14 @@ int main(int argc, char *argv[]) {
 	for (int i = 1; i < argc; ++i) {
 		if (file_included(argv[i])) {
 			filename = argv[i];
-		} else if (!file_included(argv[i])) {
-			Frontend::Exception(fileError, argv[i]);
-			return EX_DATAERR;
 		} else if (argv[i] == std::string("-o") && argc == 4) {
 			frontend.outputFileName = std::string(argv[++i]);
 		} else if (argv[i] == std::string("-o") && argc <= 3) {
 			Frontend::Exception(outFileError, argv[i]);
-			return EX_DATAERR;
-		} else {
-			Frontend::Exception(argError, argv[i]);
 			return EX_USAGE;
+		} else {
+			Frontend::Exception(fileError, argv[i]);
+			return EX_DATAERR;
 		}
 	}
 
